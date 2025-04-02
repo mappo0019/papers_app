56d8e9d1d57a7d952ffe6912fe2b0ae24ee89e8b
Miguel <mappo0019@correo.ugr.es>
2025-04-02 18:54:38 +0200

    archivod e grafo creado a ver que tal

--- src/Pages/ProjectList.jsx
+++ src/Pages/ProjectList.jsx
@@ -1,65 +1,44 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import * as d3 from "d3";
-function ProjectList(props) {
-      const {id} = useParams();
+import "../styles/ProjectList.css";
 
-      const [data, setData] = useState([]); 
-      var data2 = []; 
-      var contador = 0; 
-      
-      var margin = {top: 10, right: 30, bottom: 30, left: 40},
-      width = 400 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom;
-      
-      var svg = d3.select("#my_dataviz")
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
+ function ProjectList(props) {
+  const {id} = useParams();
 
-      //SEGUIR EN D3.JSON QUE NO SE QUE HACE
+  const [data, setData] = useState([]); 
 
-      const fetchData = async () => {
-        try {
-         
-          const response = await fetch(`http://localhost:5154/api/papers/us?user=${id}`);
-          const result = await response.json();
+  //AHORA TOCA PROBAR A VER SI PINTA EL GRAFO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-          for (let i = 0; i < await result.raw.length; i++){
-            if(contador < 149){
-              data2.push(await JSON.parse(await result.raw[i])) 
-              contador++;
-              setData(data2);
-            }
-          }
+  const fetchData = async () => {
+    try {
+     
+      const response = await fetch(`http://localhost:5154/api/graphData/2ee6e14c18a35902dd3a9f69`);
+      const result = await response.json();
+      console.log(await result.authors);
+      setData(await result);
 
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        }
-      };
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
-      useEffect(() => {
-        fetchData();
-
-        
-      }, []);
-
+        useEffect(() => {
+          fetchData(); 
+          console.log(data); 
+        }, []);
 
         return (
           <>
             <div>
                 <h3>Papers de {id}  </h3>
-                {data.length === 0 ? 
-                (<p> Este usuario no ha publicado ningún paper en OpenAlex</p>) : 
-                data.map((resp)=>(
-                  <li>{resp.title} <a href={resp.id}> Enlace </a> </li> 
-                ))}
             </div>
 
-            <div id="my_dataviz"></div>
+            {data.length === 0 ? 
+                (<p> Este usuario no ha publicado ningún paper en OpenAlex</p>) : 
+                data.authors.map((resp)=>(
+                  <li>{resp.name} </li> 
+                ))}
           </>
         );
   }
